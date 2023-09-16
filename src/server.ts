aa712b30a3a5b49f7090fa2df0ad2526418211a4
Allan Gomes <allanhouse2011@gmail.com>
2023-09-16 17:17:02 -0300

    deploy ajuste

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,6 @@ import { getAllPromptsRoute } from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
 import { createTranscriptionRoute } from './routes/create-transcription'
 import { generateAiCompletionRoute } from './routes/generate-ai-completion'
-const { execSync } = require('child_process');
-
-try {
-  // Execute o comando 'prisma generate' durante o build
-  execSync('npx prisma generate', { stdio: 'inherit' });
-} catch (error) {
-  console.error('Erro ao gerar o Prisma Client:', error);
-  process.exit(1);
-}
 
 const app = fastify()
 app.register(fastifyCors, {
